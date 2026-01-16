34e75fc58a036b7e4bdc1443d688bcb95ffefd79
DinizAlmeida <80523514+DinizAlmeida@users.noreply.github.com>
2026-01-15 21:03:26 -0300

    Add service worker for file caching
    Implement service worker for caching application files.

---
+++ Sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "pasta-emagrecimento-premium-v1";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./sw.js",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
